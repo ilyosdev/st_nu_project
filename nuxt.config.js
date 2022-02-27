6d8325f7a0b082939a48bd82c156926dc2826f5f
Muhammadjon Polvanov <misha.polvanov2706@gmail.com>
2022-02-27 23:52:35 +0500

    nuxt.config file changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -94,13 +94,6 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en',
-    },
-  },
-
   render: {
     resourceHints: false,
   },
